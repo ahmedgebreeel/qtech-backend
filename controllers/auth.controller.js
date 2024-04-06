6bc2e872302f597035e57854315bd362d9155601
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-04-06 22:11:09 +0200

    generate token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,12 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/users.model');
+
+
+const generateToken = (userId)=>{
+    return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '1d'});  // expires in one day
+}
+
+
 
 const loginController = async(req, res)=>{
     try {
@@ -9,11 +17,22 @@ const loginController = async(req, res)=>{
             if (err) {
                 console.error("Error retrieving user:", err);
                 return res.status(500).json({ message: "Internal Server Error" });
-            }
-
-            console.log(user);
-            return res.status(200).json(user);
+            } 
         });
+
+        // check if the password correct
+        const isPasswordCorrect = password === user?.password ;
+
+
+        if (!user || isPasswordCorrect) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+            // Generate a token with user
+            generateToken(user.id);
+            
+            return res.status(200).json({ message: "Login successful", user });
+        
     } catch (error) {
         console.log("error in loginController", error);
        return res.status(500).json({message: error.message});
